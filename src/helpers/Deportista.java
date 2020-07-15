66708ac87fd4b861438ffc3503c5c835d40ae319
carlos <carlos@KarenFlores>
2020-07-15 14:18:35 -0600

    Clase hecha por Karen Mendoza

---
+++ src/helpers/Deportista.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helpers;
+
+/**
+ *
+ * @author Karen Mendoza
+ */
+public class Deportista extends personas {
+    
+
+    public Deportista(){
+        establecernombre("Norma");
+        establecerapellido("Gomez");
+        estableceratipo_de_sangre("O-");
+        estableceraID("101455");
+        estableceraedad("17");
+    
+    
+}
+
+}
